22bbc1835fb73624f8c2c38e65b7b51cd129afe2
Aleksej579 <aleksej.eremenko579@gmail.com>
2021-08-31 14:58:12 +0300

    Changing the text color globally

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// var gulp = require('gulp');
+// var sass = require('gulp-sass')(require('sass'));
+// const { src, dest } = require('gulp');
+// const { watch, series } = require('gulp');
+
+// function css() {
+//     return src('./assets/*.scss')
+//         .pipe(gulp.dest('./assets/'));
+// }
+
+// exports.default = function () {
+//     watch('./assets/*.scss', css);
+// }
