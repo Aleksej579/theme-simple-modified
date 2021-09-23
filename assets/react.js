0abe2f31d97c493cae622f2608e4ed4913150e61
Aleksej579 <aleksej.eremenko579@gmail.com>
2021-09-23 18:47:46 +0300

    Create a carousel widget on the home page

--- assets/react.js
+++ assets/react.js
@@ -26,12 +26,13 @@
 
 $(document).ready(function () {
 
+    // code for owl.carusel start
     $(".owl-carousel").owlCarousel();
-
     $('#ex1').modal({
         fadeDuration: 250,
         fadeDelay: 0.80
     });
+    // code for owl.carusel end
 
     function sayHi() {
         $.modal.close();
