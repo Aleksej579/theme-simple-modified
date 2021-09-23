83a7e5b5f374378785511b5c61768395978a3e90
Aleksej579 <aleksej.eremenko579@gmail.com>
2021-09-23 18:26:18 +0300

    Display a modal pop-up 10 seconds

--- assets/react.js
+++ assets/react.js
@@ -1,3 +1,29 @@
+// $(document).ready(function () {
+
+//     $(".owl-carousel").owlCarousel();
+
+//     $('#ex1').modal({
+//         fadeDuration: 250,
+//         fadeDelay: 0.80
+//     });
+
+//     function sayHi() {
+//         $.modal.close();
+//     }
+
+//     setTimeout(sayHi, 10000);
+
+//     $({ Counter: 0 }).animate({
+//         Counter: $('.Single').text()
+//     }, {
+//         duration: 62000,
+//         easing: 'swing',
+//         step: function () {
+//             $('.Single').text(Math.ceil(this.Counter));
+//         }
+//     });
+// });
+
 $(document).ready(function () {
 
     $(".owl-carousel").owlCarousel();
@@ -22,8 +48,6 @@ $(document).ready(function () {
             $('.Single').text(Math.ceil(this.Counter));
         }
     });
-
-
-
-
 });
+
+
